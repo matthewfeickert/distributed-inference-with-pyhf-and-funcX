5a4027cb11168ea999969245b63de67440ef6e8e
Matthew Feickert <matthew.feickert@cern.ch>
2021-02-27 16:31:33 -0600

    Add temp comments

--- fit_analysis.py
+++ fit_analysis.py
@@ -34,6 +34,7 @@ def infer_hypotest(workspace, metadata, doc):
         "metadata": metadata,
         "CLs_obs": float(
             # pyhf.infer.hypotest(test_poi, data, model, test_stat="qtilde")
+            # Use v0.5.4 API until endpoint fixed
             pyhf.infer.hypotest(test_poi, data, model, qtilde=True)
         ),
         "Fit-Time": time.time() - tick,
@@ -49,20 +50,23 @@ def main():
     # if not Path("1Lbb-pallet").exists():
     #     download("https://doi.org/10.17182/hepdata.90607.v3/r3", "1Lbb-pallet")
 
-    # pallet_name = "InclSS3L-pallet"
-    # pallet_url = "https://www.hepdata.net/record/resource/1935157?view=true"
-    # analysis_name = "Rpc2L0b"
-    pallet_name = "1Lbb-pallet"
-    pallet_url = "https://www.hepdata.net/record/resource/1408476?view=true"
-    analysis_name = None
+    input_prefix = "input"
+    pallet_name = "InclSS3L-pallet"
+    pallet_url = "https://www.hepdata.net/record/resource/1935157?view=true"
+    analysis_name = "Rpc2L0b"
+    # input_prefix = "input"
+    # pallet_name = "1Lbb-pallet"
+    # pallet_url = "https://www.hepdata.net/record/resource/1408476?view=true"
+    # analysis_name = None
 
-    patchset_name = pallet_name + "/"
-    if analysis_name is not None:
-        patchset_name += f"{analysis_name}_"
+    pallet_path = Path(input_prefix).joinpath(pallet_name)
+    analysis_prefix_str = "" if analysis_name is None else f"{analysis_name}_"
 
-    if not Path(pallet_name).exists():
-        download(pallet_url, pallet_name)
-    with open(f"{patchset_name}BkgOnly.json") as bkgonly_json:
+    if not pallet_path.exists():
+        download(pallet_url, pallet_path)
+    with open(
+        pallet_path.joinpath(f"{analysis_prefix_str}BkgOnly.json")
+    ) as bkgonly_json:
         bkgonly_workspace = json.load(bkgonly_json)
 
     # with open(f"{patchset_name}patchset.json") as patchset_json:
@@ -76,70 +80,75 @@ def main():
     #     print(patch.metadata)
     # ##
 
-    pyhf_endpoint = "a727e996-7836-4bec-9fa2-44ebf7ca5302"
+    with open("endpoint_id.txt") as endpoint_file:
+        pyhf_endpoint = endpoint_file.read()
+
+    print(pyhf_endpoint)
 
     # Initialize funcX client
     fxc = FuncXClient()
     fxc.max_requests = 200
 
-    # register and execute background only workspace
-    prepare_func = fxc.register_function(prepare_workspace)
-    infer_func = fxc.register_function(infer_hypotest)
-    prepare_task = fxc.run(
-        bkgonly_workspace, endpoint_id=pyhf_endpoint, function_id=prepare_func
-    )
+    # # register and execute background only workspace
+    # prepare_func = fxc.register_function(prepare_workspace)
+    # infer_func = fxc.register_function(infer_hypotest)
+    # prepare_task = fxc.run(
+    #     bkgonly_workspace, endpoint_id=pyhf_endpoint, function_id=prepare_func
+    # )
 
-    # While this cooks, let's read in the patch set
-    # patchset = None
-    # with open("1Lbb-pallet/patchset.json", "r") as readfile:
-    #     patchset = json.load(readfile)
-    # patch = patchset["patches"][0]
-    # name = patch["metadata"]["name"]
+    # # While this cooks, let's read in the patch set
+    # # patchset = None
+    # # with open("1Lbb-pallet/patchset.json", "r") as readfile:
+    # #     patchset = json.load(readfile)
+    # # patch = patchset["patches"][0]
+    # # name = patch["metadata"]["name"]
 
-    with open(f"{patchset_name}patchset.json") as patchset_json:
+    with open(
+        pallet_path.joinpath(f"{analysis_prefix_str}patchset.json")
+    ) as patchset_json:
         patchset = pyhf.PatchSet(json.load(patchset_json))
     patch = patchset.patches[0]
 
-    workspace = None
-    while not workspace:
-        try:
-            workspace = fxc.get_result(prepare_task)
-        except Exception as excep:
-            print(f"prepare: {excep}")
-            sleep(15)
+    # workspace = None
+    # while not workspace:
+    #     try:
+    #         workspace = fxc.get_result(prepare_task)
+    #     except Exception as excep:
+    #         print(f"prepare: {excep}")
+    #         sleep(15)
 
-    print("--------------------")
-    print(workspace)
+    # print("--------------------")
+    # print(workspace)
 
-    # NUM_RUNS = len(patchset.patches)
-    NUM_RUNS = 5
-    tasks = {}
-    for i in range(NUM_RUNS):
-        patch = patchset.patches[i]
-        task_id = fxc.run(
-            workspace,
-            patch.metadata,
-            patch,
-            endpoint_id=pyhf_endpoint,
-            function_id=infer_func,
-        )
-        tasks[patch.name] = {"id": task_id, "result": None}
+    # # NUM_RUNS = len(patchset.patches)
+    # NUM_RUNS = 5
+    # tasks = {}
+    # for i in range(NUM_RUNS):
+    #     patch = patchset.patches[i]
+    #     task_id = fxc.run(
+    #         workspace,
+    #         patch.metadata,
+    #         patch,
+    #         endpoint_id=pyhf_endpoint,
+    #         function_id=infer_func,
+    #     )
+    #     tasks[patch.name] = {"id": task_id, "result": None}
 
-    while count_complete(tasks.values()) < NUM_RUNS:
-        for task in tasks.keys():
-            if not tasks[task]["result"]:
-                try:
-                    result = fxc.get_result(tasks[task]["id"])
-                    print(
-                        f"Task {task} complete, there are {count_complete(tasks.values())} results now"
-                    )
-                    tasks[task]["result"] = result
-                except Exception as excep:
-                    print(excep)
-                    sleep(15)
+    # while count_complete(tasks.values()) < NUM_RUNS:
+    #     for task in tasks.keys():
+    #         if not tasks[task]["result"]:
+    #             try:
+    #                 result = fxc.get_result(tasks[task]["id"])
+    #                 print(
+    #                     f"Task {task} complete, there are {count_complete(tasks.values())} results now"
+    #                 )
+    #                 tasks[task]["result"] = result
+    #             except Exception as excep:
+    #                 print(excep)
+    #                 sleep(15)
 
-    print("--------------------")
-    print(tasks.values())
+    # print("--------------------")
+    # print(tasks.values())
 
 
 if __name__ == "__main__":
