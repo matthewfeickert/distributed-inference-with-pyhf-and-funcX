57d179d477bb1ee129ccb5a6998ba80d9dea08a0
Matthew Feickert <matthew.feickert@cern.ch>
2021-02-27 18:29:13 -0600

    use patch API

--- fit_analysis.py
+++ fit_analysis.py
@@ -82,7 +82,7 @@ def main(args):
         pallet_path.joinpath(f"{analysis_prefix_str}patchset.json")
     ) as patchset_json:
         patchset = pyhf.PatchSet(json.load(patchset_json))
-    patch = patchset.patches[0]
+    patch = patchset.patches[0].patch
 
     workspace = None
     while not workspace:
