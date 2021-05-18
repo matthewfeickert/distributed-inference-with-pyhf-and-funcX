d66e2ad030559584835c5067d104e5cae8cacb3f
BenGalewsky <BenGalewsky@users.noreply.github.com>
2021-05-18 11:17:47 -0500

    Bug/fix expanse config (#13)
    * Move expanse config to correct dir
    
    * Update expanse funcx config
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- funcx/expanse_config.py
+++ funcx/expanse_config.py
@@ -7,7 +7,7 @@ from parsl.addresses import address_by_hostname
 # PLEASE UPDATE user_opts BEFORE USE
 user_opts = {
     "expanse": {
-        "worker_init": "source ~/setup_expanse_funcx_test_env.sh",
+        "worker_init": "source ~/setup_funcx_test_env.sh",
         "scheduler_options": "#SBATCH --gpus=1",
     }
 }
@@ -18,11 +18,12 @@ config = Config(
             label="Expanse_GPU",
             address=address_by_hostname(),
             provider=SlurmProvider(
-                "gpu-debug",  # Partition / QOS
+                "gpu",  # Partition / QOS
                 account="nsa106",
                 nodes_per_block=1,
+                max_blocks=4,
                 init_blocks=1,
-                mem_per_node=20,
+                mem_per_node=96,
                 # string to prepend to #SBATCH blocks in the submit
                 # script to the scheduler eg: '#SBATCH --constraint=knl,quad,cache'
                 scheduler_options=user_opts["expanse"]["scheduler_options"],
@@ -31,6 +32,7 @@ config = Config(
                 worker_init=user_opts["expanse"]["worker_init"],
                 launcher=SrunLauncher(),
                 walltime="00:10:00",
+                # Slurm scheduler on Cori can be slow at times,
                 # increase the command timeouts
                 cmd_timeout=120,
             ),
